a2fe03b02ffe0c320752672837042630d216aa64
Tyler Mueller <muelltyl@gmail.com>
2023-06-04 13:23:04 -0500

    adds getProjectTopLevel

---
+++ src/getProjectTopLevel.ts
@@ -0,0 +1,19 @@
+export const gitPath = `${getProjectTopLevelSync()}/.git`;
+
+export function getProjectTopLevelSync(): string | Error {
+    let currentDir = Deno.cwd();
+
+    while (currentDir !== '/') {
+        try {
+           const fileInfo = Deno.statSync(`${currentDir}/.git`);
+           if(fileInfo.isDirectory) {
+            return currentDir;
+           }
+        } catch (e) {
+            // Directory not found, continue traversing up
+        }
+
+        currentDir = `${currentDir}/..`;
+    }
+    return new Error('Failed to find the top level of project directory');
+}
--- src/mod.ts
+++ src/mod.ts
@@ -1,1 +1,2 @@
 export * from "./getCurrentCommitHash.ts";
+export * from './getProjectTopLevel.ts'
